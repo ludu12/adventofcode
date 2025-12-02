2c61b45073d87891142cb042c9550f384f868060
Luke Dunscombe <dunscombe1@gmail.com>
2025-12-02 08:10:23 -0600

    Starting day 2

--- src/_2025/mod.rs
+++ src/_2025/mod.rs
@@ -1,9 +1,11 @@
 #![allow(unused_variables)] // Disables unused_variables warnings for the entire crate
 pub mod one;
+pub mod two;
 
 pub fn run_day(day: u32) {
     match day {
         1 => one::run(),
+        2 => two::run(),
         _ => println!("Invalid advent day! \"{}\"", day),
     }
 }
---
+++ src/_2025/two/mod.rs
@@ -0,0 +1,71 @@
+pub fn run() {
+    let input = include_str!("input.txt");
+    let part1 = process(input, false);
+    println!("Part1: {}", part1.to_string());
+    let part2 = process(input, true);
+    println!("Part2: {}", part2.to_string());
+}
+
+fn process(input: &str, part2: bool) -> i32 {
+    let mut lands_on_zero = 0;
+    let mut clicks_on_zero = 0;
+    let mut position = 50;
+
+    input.lines().for_each(|line| {
+        let mut s = line.chars();
+        let direction = s.next().unwrap();
+        let distance: i32 = s.collect::<String>().parse().unwrap();
+
+        clicks_on_zero += distance / 100;
+
+        let real_distance = distance % 100;
+        let sign = if direction == 'L' { -1 } else { 1 };
+        let mut new_position = position + sign * real_distance;
+
+        let remainder = new_position % 100;
+        new_position = if new_position < 0 { 100 + remainder } else { remainder };
+
+        match direction {
+            'L' => {
+                if position < new_position && position != 0 {
+                    clicks_on_zero += 1;
+                }
+            }
+            'R' => {
+                if position > new_position && new_position != 0 {
+                    clicks_on_zero += 1;
+                }
+            }
+            _ => panic!("Invalid direction"),
+        }
+
+        position = new_position;
+        if position == 0 {
+            clicks_on_zero += 1;
+            lands_on_zero += 1;
+        }
+    });
+
+    if part2 { clicks_on_zero } else {  lands_on_zero }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part1() {
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124";
+        assert_eq!(3, process(input, false));
+    }
+
+    #[test]
+    fn part2() {
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124";
+        assert_eq!(6, process(input, true));
+    }
+}
