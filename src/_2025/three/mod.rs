d6784815b76210f0ba94e72b292bc1168066c5c9
Luke Dunscombe <dunscombe1@gmail.com>
2025-12-03 12:16:26 -0600

    day 3

--- src/_2025/three/mod.rs
+++ src/_2025/three/mod.rs
@@ -12,7 +12,7 @@ fn find_max_index(numbers: &Vec<u32>, left: usize, right: usize) -> usize {
     let start = 0 + left;
     let end = numbers.len() - right;
 
-    for i in (start..end).rev() {
+    for i in start..end {
         if numbers[i] > numbers[max] {
             max = i
         }
@@ -20,7 +20,7 @@ fn find_max_index(numbers: &Vec<u32>, left: usize, right: usize) -> usize {
     max
 }
 
-fn process(input: &str, part2: bool) -> u32  {
+fn process(input: &str, part2: bool) -> u64  {
 
     let mut count = 0;
     input.lines().for_each(|line| {
@@ -31,11 +31,15 @@ fn process(input: &str, part2: bool) -> u32  {
             .filter_map(|c| c.to_digit(10))
             .collect();
 
-        let first = find_max_index(&numbers, 0, 1);
-        let second = find_max_index(&numbers, first + 1, 0);
+        let total = if part2 { 12 } else { 2 };
 
-        count += numbers[first] * 10 + numbers[second];
-
+        let mut leftmost = 0;
+        for i in (0..total).rev() {
+            let max = find_max_index(&numbers, leftmost, total - (total - i));
+            leftmost = max + 1;
+            let pow = 10_u64.pow(i as u32);
+            count += (numbers[max] as u64) * pow;
+        }
     });
 
     count
@@ -57,11 +61,18 @@ mod test {
     }
 
     #[test]
+    fn part1b() {
+        let input = "3722443164324852429541739322454443622742537425744313396455466849784737627295682866595242427454396354
+2322233122227224822212323658212222255122122243112292222221242252322222312243522322124211222122322225";
+        assert_eq!(194, process(input, false));
+    }
+
+    #[test]
     fn part2() {
         let input = "987654321111111
 811111111111119
 234234234234278
 818181911112111";
-        assert_eq!(357, process(input, true));
+        assert_eq!(3121910778619, process(input, true));
     }
 }
