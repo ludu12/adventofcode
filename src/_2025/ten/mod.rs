4a3abb18e1c7bf785e1e485a9b10ff08579bb9d0
Luke Dunscombe <dunscombe1@gmail.com>
2025-12-10 12:27:42 -0600

    Part 2 day 10

--- src/_2025/ten/mod.rs
+++ src/_2025/ten/mod.rs
@@ -10,6 +10,8 @@ use std::collections::{HashMap, HashSet, VecDeque};
 use std::fmt;
 use std::hash::{DefaultHasher, Hash, Hasher};
 use std::os::unix::raw::off_t;
+use z3::ast::Int;
+use z3::{Optimize, SatResult};
 
 pub fn run() {
     let input = include_str!("input.txt");
@@ -23,6 +25,7 @@ pub fn run() {
 struct Machine {
     lights: u16,
     buttons: Vec<u16>,
+    raw_buttons: Vec<Vec<usize>>,
     joltage: Vec<u16>,
 }
 
@@ -90,6 +93,23 @@ fn parse_machine(line: &str) -> Machine {
         })
         .collect_vec();
 
+    let raw_buttons = buttons
+        .trim()
+        .split(" ")
+        .map(|s| {
+            let b_re = Regex::new(r"\((.*?)\)").unwrap();
+            b_re
+                .captures(s)
+                .unwrap()
+                .get(1)
+                .unwrap()
+                .as_str()
+                .split(",")
+                .map(|b| b.parse::<usize>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
     let v_re = Regex::new(r"\{(.*?)\}").unwrap();
     let v_joltage = v_re
         .captures(joltage)
@@ -104,6 +124,7 @@ fn parse_machine(line: &str) -> Machine {
     Machine {
         lights: n_lights,
         buttons: v_buttons,
+        raw_buttons,
         joltage: v_joltage,
     }
 }
@@ -126,6 +147,46 @@ fn process_machine(machine: &Machine) -> usize {
     0
 }
 
+
+fn process_machine_part2(machine: &Machine) -> usize {
+    let opt = Optimize::new();
+
+    // Total presses which we will minimize
+    let total = Int::fresh_const("total");
+
+    // Create integer variables for each button press
+    let presses = (0..machine.buttons.len()).map(|i| {
+        Int::fresh_const(&format!("p{}", i))
+    }).collect_vec();
+
+    // Assert each press is non-negative
+    presses.iter().for_each(|b| opt.assert(&b.ge(0)));
+
+    for (pos, &target) in machine.joltage.iter().enumerate() {
+        let mut terms = Vec::new();
+
+        for (idx, button) in machine.raw_buttons.iter().enumerate() {
+            if button.contains(&pos) {
+                terms.push(presses[idx].clone());
+            }
+        }
+        let sum = Int::add(&terms.iter().collect::<Vec<&Int>>());
+        opt.assert(&sum.eq(Int::from_u64(target as u64)));
+    }
+    opt.assert(&total.eq(Int::add(&presses)));
+    opt.minimize(&total);
+
+    match opt.check(&[]) {
+        SatResult::Sat => opt
+            .get_model()
+            .unwrap()
+            .eval(&total, true)
+            .and_then(|t| t.as_u64())
+            .unwrap() as usize,
+        _              => panic!("No solution found"),
+    }
+}
+
 fn process_part1(input: &str) -> i64 {
     let machines = input
         .lines()
@@ -135,8 +196,13 @@ fn process_part1(input: &str) -> i64 {
     machines.iter().map(|machine| process_machine(machine) as i64).sum()
 }
 
-fn process_part2(input: &str) -> u32 {
-    24
+fn process_part2(input: &str) -> i64 {
+    let machines = input
+        .lines()
+        .map(|line| parse_machine(line))
+        .collect_vec();
+
+    machines.iter().map(|machine| process_machine_part2(machine) as i64).sum()
 }
 
 #[cfg(test)]
@@ -156,6 +222,6 @@ mod test {
         let input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}";
-        assert_eq!(24, process_part2(input));
+        assert_eq!(33, process_part2(input));
     }
 }
